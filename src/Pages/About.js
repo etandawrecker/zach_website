98d53e7f4eafb9c16865e36e6537d423019adaac
mayihavecrumbofcoochieplz <ezhou24@wisc.edu>
2024-02-18 16:57:33 -0600

    fixed about me

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -21,11 +21,7 @@ const About = () => {
         <p>Angerthal is primarily interested in the realms of fine art photography, commercial<br/> 
           photography, and documentary photography. He worked as the photographer at his summer camp<br/>  
           — North Woods Camp of YMCA of Greater Boston — during the summer of 2022.<br/>  
-          He is currently enrolled in <i>Life Sciences Communication 450: Documentary Photography for<br/>  
-          the Sciences</i> at UW-Madison, a course that educates students in the art of visual storytelling<br/>  
-          and fosters a photographic mindset for effectively communicating topics related to science,<br/>  
-          health, and the environment. In this course, he examines the impact of social documentary<br/>  
-          photography, with assignments focused on developing a portfolio of documentary photography.</p>
+          His projects to date include <i>North Woods 2022</i>, <i>Spring in Spain</i> (2023), and <i>The Rockies</i> (2024).</p>
       </div>
     </div>
   );
