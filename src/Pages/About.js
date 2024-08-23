41af7ef18cccd8998672007eafd639bbc30ba7ad
mayihavecrumbofcoochieplz <ezhou24@wisc.edu>
2024-08-23 14:40:36 -0400

    about page changes, removed breaks

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -15,15 +15,15 @@ const About = () => {
           environment, science, and health. This course reinvigorated his passion for photography and inspired him to create on a 
           personal and professional level.</p>
 
-        <p>Home is a word unfamiliar to Angerthal, as he was raised as a military brat.<br/>
-          His passion for photography began when living in Quito, Ecuador as a young boy. In awe of the beautiful<br/>
-          landscapes and fascinating culture, he felt compelled to get his first camera. Ever since, he has<br/>
+        <p>Home is a word unfamiliar to Angerthal, as he was raised as a military brat.
+          His passion for photography began when living in Quito, Ecuador as a young boy. In awe of the beautiful
+          landscapes and fascinating culture, he felt compelled to get his first camera. Ever since, he has
           enjoyed the act of capturing his everyday moments wherever he goes. </p>
 
         <p>Angerthal is primarily interested in the realms of fine art photography and commercial photography, 
           including but not limited to, events, establishments (restaurants, stores, etc.), and product(s). 
-          He worked as the photographer at his summer camp<br/>  
-          — North Woods Camp of YMCA of Greater Boston — during the summer of 2022.<br/>  
+          He worked as the photographer at his summer camp  
+          — North Woods Camp of YMCA of Greater Boston — during the summer of 2022.  
           His projects to date include <i>North Woods 2022</i>, <i>Spring in Spain</i> (2023), and <i>The Rockies</i> (2024).</p>
       </div>
     </div>
