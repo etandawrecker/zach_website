6ff8e1314cd21a9221676392d663bcbe40fc6aa0
mayihavecrumbofcoochieplz <ezhou24@wisc.edu>
2024-09-02 15:24:19 -0500

    added artist name and location to reinterpretation

--- src/Pages/Personal/Reinterpretation.js
+++ src/Pages/Personal/Reinterpretation.js
@@ -20,11 +20,13 @@ const Reinterpretation = () => {
             title1: 'Coming and Going',
             image1: 'Reinterpretation Photos/Coming and Going.png', // Adjust the path based on your directory structure
             alt1: 'Photo of a mural of a bee on top of a sunflower',
+            description: "Artist: Willey (2019) in Smithsonian National Zoological Park"
         }, 
         {
             title1: 'Letting Go',
             image1: 'Reinterpretation Photos/Letting Go.png',
-            alt1: 'A mural of children sitting on a tree branch and birds are flying over them'
+            alt1: 'A mural of children sitting on a tree branch and birds are flying over them',
+            description: "Artist: Ana Cristina Dias in Lisbon, Portugal"
         }
       ];
 
@@ -46,6 +48,7 @@ const Reinterpretation = () => {
                 title1={slides[currentSlide].title1}
                 image1={slides[currentSlide].image1}
                 alt1={slides[currentSlide].alt1}
+                description={slides[currentSlide].description}
             />
 
 
